9c9d621a1bd0289a23f26dfa3664fa6906d58c36
Carl Richter <richter@september-werbeagentur.de>
2019-09-26 16:02:35 +0200

    fix associateslist palette

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,7 +1,7 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['associatesfinder'] = '{title_legend},name,type;{redirect_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;';
-$GLOBALS['TL_DCA']['tl_module']['palettes']['associateslist'] = '{title_legend},name,type;{swa_result_legend},swa_noResult;{redirect_legend},jumpTo;{protected_legend:hide},protected;{swa_distance_matrix_legend},distance_matrix_key;{expert_legend:hide},guests,cssID;';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['associateslist'] = '{title_legend},name,type;{swa_result_legend},swa_noResult;{redirect_legend},jumpTo;{swa_distance_matrix_legend},swa_distance_matrix_key;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;';
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['swa_noResult'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_module']['swa_noResult'],
